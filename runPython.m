dfa1a0dc34f073df962c6156d8b3da1416990694
檜山 徹 <robot_toru@outlook.jp>
2018-09-19 23:55:27 +0900

    特に見られても構わないが、わかりやすくするためにディレクトリを一部改変していたので、それを自分ようにしたMATLAB コード

--- runPython.m
+++ runPython.m
@@ -1,3 +1,4 @@
-cd C:\Anaconda3
-command = "python C:\test.py 2 3"
+cd C:\Users\robot\Anaconda3\envs\tensorflow-gpu
+command = "python C:\Users\robot\OneDrive\Desktop\Laboratory\Python\forHatena\test.py 2 3"
 status = dos(command)
+cd C:\Users\robot\OneDrive\Desktop\Laboratory\Python\forHatena
